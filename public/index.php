dbc2f4ad0912d1181093e6a6e359d06d0fe60046
laxatzi <laxatzi@yahoo.com>
2025-01-23 15:04:14 +0200

    add 404 page to router

--- public/index.php
+++ public/index.php
@@ -2,5 +2,13 @@
   $url = $_GET['url'] ?? 'home';
   $url = explode('/', $url);
 
-  echo "<pre>";
-  print_r($url);
+  $page_name = trim($url[0]);
+  $filename = "../app/pages/".$page_name.".php";
+
+  if (file_exists($filename)) {
+     require_once $filename;
+  } else {
+    require_once "../app/pages/404.php";
+  }
+
+  //print_r($url);
