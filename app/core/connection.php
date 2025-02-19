8e16c6e511900b5c3149fbb036817369a8ed13d8
laxatzi <laxatzi@yahoo.com>
2025-02-19 12:51:43 +0200

    define constants for localhost

--- app/core/connection.php
+++ app/core/connection.php
@@ -1,10 +1,19 @@
 <?php
 
-define('DBUSER', 'ROOT');
-define('DBPASS', "");
-define('DBNAME', "my_blog_db");
-define('DBHOST', 'localhost');
 
-// make a connection
+// reduce back and forth changing of the same code while working in a local environment
 
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
+  define('DBUSER', 'ROOT');
+  define('DBPASS', "");
+  define('DBNAME', "my_blog_db");
+  define('DBHOST', 'localhost');
+
+}else {
+  define('DBUSER', 'ROOT');
+  define('DBPASS', "");
+  define('DBNAME', "my_blog_db");
+  define('DBHOST', 'localhost');
+
+}
 
