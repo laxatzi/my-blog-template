30db85a3a1f8fa3f8e85bb4d3a8c5a084779c540
laxatzi <laxatzi@yahoo.com>
2025-02-19 15:16:41 +0200

    create the connection into create_tables function

--- app/core/functions.php
+++ app/core/functions.php
@@ -1,1 +1,10 @@
 <?php
+  // create a function that creates tables | when function called it will make a db connection and run the tables that we need
+  create_tables();
+
+  function create_tables() {
+    $string = "mysql:hostname=localhost;";
+    $con = new PDO($string, DBUSER, DBPASS);
+
+    print_r($con);
+  }
