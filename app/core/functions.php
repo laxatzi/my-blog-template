fb0bc5f9103a170ae4e72af49a993923929c3d50
laxatzi <laxatzi@yahoo.com>
2025-02-20 16:05:22 +0200

    create database table users()|

--- app/core/functions.php
+++ app/core/functions.php
@@ -6,8 +6,32 @@
 	$str = "mysql:hostname=localhost;";
     $conn = new PDO($str, DBUSER, DBPASS);
 
-    $query = "create database if not exists ".DBNAME;
+    $query = "CREATE DATABASE IF NOT EXISTS ".DBNAME;
 
-    $stm = $conn->prepare($query);
-    $stm->execute();
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+
+    // Create tables
+
+    $query = "use ". DBNAME;
+	  $stm = $conn->prepare($query);
+	  $stm->execute();
+
+    // Users table
+    $query = "CREATE TABLE IF NOT EXISTS users(
+      id int primary key auto_increment,
+		  username varchar(50) not null,
+		  email varchar(100) not null,
+		  password varchar(255) not null,
+		  image varchar(1024) null,
+		  date datetime default current_timestamp,
+		  role varchar(10) not null,
+
+		  key username (username),
+		  key email (email)
+
+    )";
+
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
   }
