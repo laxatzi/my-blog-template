40dde9ffb59d799722675a1850c1937633ae3ee3
laxatzi <laxatzi@yahoo.com>
2025-02-20 16:20:06 +0200

    create categories table

--- app/core/functions.php
+++ app/core/functions.php
@@ -6,16 +6,20 @@
 	$str = "mysql:hostname=localhost;";
     $conn = new PDO($str, DBUSER, DBPASS);
 
+// Create database
+
     $query = "CREATE DATABASE IF NOT EXISTS ".DBNAME;
 
     $stmt = $conn->prepare($query);
     $stmt->execute();
 
-    // Create tables
+// Select the database to use
 
     $query = "use ". DBNAME;
 	  $stm = $conn->prepare($query);
 	  $stm->execute();
+
+// Create tables
 
     // Users table
     $query = "CREATE TABLE IF NOT EXISTS users(
@@ -34,4 +38,19 @@
 
     $stmt = $conn->prepare($query);
     $stmt->execute();
+
+    // categories table
+	$query = "CREATE TABLE IF NOT EXISTS categories(
+
+		id int primary key auto_increment,
+		category varchar(50) not null,
+		slug varchar(100) not null,
+		disabled tinyint default 0,
+
+		key slug (slug),
+		key category (category)
+
+	)";
+	$stm = $conn->prepare($query);
+	$stm->execute();
   }
