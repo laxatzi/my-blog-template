cf0dd05a1c761986bf7a0da2408a03c4c7993486
laxatzi <laxatzi@yahoo.com>
2025-03-20 13:02:01 +0200

    lowercase urls

--- public/index.php
+++ public/index.php
@@ -1,8 +1,11 @@
 <?php
+  session_start();
+
   // init is the only file we require here.
   require "../app/core/init.php";
 
   $url = $_GET['url'] ?? 'home';
+  $url = strtolower($url);
   $url = explode('/', $url);
 
   $page_name = trim($url[0]);
@@ -14,4 +17,4 @@
     require_once "../app/pages/404.php";
   }
 
-  //print_r($url);
+
