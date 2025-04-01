526640caa51e428356fd7d6b51a0966b071f990d
laxatzi <laxatzi@yahoo.com>
2025-04-01 12:55:43 +0300

    Add js script src

--- app/pages/register.php
+++ app/pages/register.php
@@ -79,6 +79,7 @@
        <!-- Custom styles for this template -->
     <link href="<?=ROOT?>/assets/css/sign-in.css" rel="stylesheet">
     <link href="<?=ROOT?>/assets/css/main.css" rel="stylesheet">
+    <script src="<?=ROOT?>/assets/js/passVisibilityToggler.js" defer></script>
 
     <style>
       .bd-placeholder-img {
@@ -349,32 +350,5 @@
   </form>
 </main>
 <script src="<?=ROOT?>/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-<script>
-  let eyeIcon = document.querySelector("#js--toggleImg");//js--toggleImg
-  let password = document.querySelector("#js--floatingPassword");//js--floatingPassword
-
-if (password) {
-    let passwordValue = password.value;
-    console.log(passwordValue);
-} else {
-    console.error("Password input field not found!");
-}
-
-
-
-const typeToggler = function(e) {
-  e.preventDefault();
-  if (password.type == "password") {
-  password.type = "text";
-  } else if (password.type == "text") {
-  password.type = "password";
-  }
-}
-
-
-  eyeIcon.addEventListener('click', typeToggler);
-
-</script>
 </body>
 </html>
